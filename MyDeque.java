6fec60a7b3100908451c80611aa52939aed00b04
fkhan00 <43051637+fkhan00@users.noreply.github.com>
2019-03-21 18:10:04 -0400

    finished remove mehtods

--- MyDeque.java
+++ MyDeque.java
@@ -1,11 +1,10 @@
 public class MyDeque<E>{
   public E[] data;
-  public int size, start, end;
+  public int size = 10;
+  public int start = 0;
+  public int end = 0;
   public MyDeque(){
     @SuppressWarnings("unchecked")
-    size = 10;
-    end = 10;
-    start = 0;
     data = (E[])new Object[10];}
 
   public MyDeque(int startingCapacity){
@@ -20,8 +19,10 @@ public class MyDeque<E>{
 
   public String toString(){
     String output = "";
-    for(int i = 0; i < data.length; i++){
-      output += data[i];}
+    for(int i = start; i < data.length; i++){
+      output += " " + data[i];}
+    for(int i = 0; i <= end; i++){
+      output += " " + data[i];}
     return output;}
 
   public E getFirst(){
@@ -31,21 +32,17 @@ public class MyDeque<E>{
     return data[end];}
 
   public void removeFirst(){
-    start = (start + 1) % (data.length);}
+    start = (start + 1) %(data.length);}
 
   public void removeLast(){
-    end = (Math.abs((end - 1)) % (data.length));}
+    if(end == 0){
+      end = data.length - 1;}
+    end = end - 1;}
 
   public void addFirst(E element){
-    if(Math.abs(end - start) == 1){
-      resize();}
-    start = (size() - start + 1) % (size());
-    data[start] = element;}
+}
 
   public void addLast(E element){
-    if(Math.abs(end - start) == 1){
-      resize();}
-    end = (end + 1) % size();
-    data[end] = element;
+
   }
 }
