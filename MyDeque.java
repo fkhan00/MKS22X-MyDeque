626abeb4b828cb827766ad3a2ff9ccc876dbe367
fkhan00 <43051637+fkhan00@users.noreply.github.com>
2019-03-21 18:33:30 -0400

    added resize method

--- MyDeque.java
+++ MyDeque.java
@@ -1,14 +1,13 @@
+@SuppressWarnings("unchecked")
 public class MyDeque<E>{
   public E[] data;
-  public int size = 10;
-  public int start = 0;
-  public int end = 0;
+  public int size, start, end;
   public MyDeque(){
-    @SuppressWarnings("unchecked")
-    data = (E[])new Object[10];}
-
+    size = 10;
+    start = 0;
+    end = 0;
+    data = (E[]) new Object[10];}
   public MyDeque(int startingCapacity){
-    @SuppressWarnings("unchecked")
     size = startingCapacity;
     end = startingCapacity;
     start = 0;
@@ -31,15 +30,19 @@ public class MyDeque<E>{
   public E getLast(){
     return data[end];}
 
-  public void removeFirst(){
+  public E removeFirst(){
+    E temp = getFirst();
     start = (start + 1) %(size());
-    size --;}
+    size --;
+    return temp;}
 
-  public void removeLast(){
+  public E removeLast(){
+    E temp = getLast();
     if(end == 0){
       end = size();}
     end = end - 1;
-    size --;}
+    size --;
+    return temp;}
 
   public void addFirst(E element){
     if(start == 0){
@@ -51,4 +54,9 @@ public class MyDeque<E>{
   public void addLast(E element){
     end = (end + 1) % (size());
     data[end] = element;}
+
+  public void resize(){
+    E[] temp = (E[])new Object[size() * 2];
+    for(int i = 0; i < data.length; i++){
+      temp[i] = data[i];}}
 }
