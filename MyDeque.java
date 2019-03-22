2402bc5687af59e8e588d667444510bc3c993947
fkhan00 <43051637+fkhan00@users.noreply.github.com>
2019-03-22 11:45:27 -0400

    tested with Ethan's driver fixed bugs

--- MyDeque.java
+++ MyDeque.java
@@ -4,10 +4,10 @@ public class MyDeque<E>{
   public E[] data;
   public int size, start, end;
   public MyDeque(){
-    size = 10;
+    size = 0;
     start = 0;
     end = 0;
-    data = (E[]) new Object[10];}
+    data = (E[]) new Object[1];}
   public MyDeque(int startingCapacity){
     size = startingCapacity;
     end = startingCapacity;
@@ -18,12 +18,20 @@ public class MyDeque<E>{
     return size;}
 
   public String toString(){
-    String output = "";
+    String output = "[";
+    if(end < start){
     for(int i = start; i < data.length; i++){
-      output += " " + data[i];}
+      if(data[i] != null){
+      output +=  data[i] + ", ";}}
     for(int i = 0; i <= end; i++){
-      output += " " + data[i];}
-    return output;}
+      if(data[i] != null){
+      output += data[i] + ", ";}}}
+    else{
+      for(int i = start; i <= end; i++){
+        if(data[i] != null){
+        output += data[i] +", ";}}
+    }
+    return output.substring(0, output.length()) + "]";}
 
   public E getFirst(){
     if(size() == 0){
@@ -39,7 +47,7 @@ public class MyDeque<E>{
     if(size() == 0){
       throw new NoSuchElementException("your double deque is empty");}
     E temp = getFirst();
-    start = (start + 1) %(size());
+    start = (start + 1) %(size() + 1);
     size --;
     return temp;}
 
@@ -56,20 +64,41 @@ public class MyDeque<E>{
   public void addFirst(E element){
     if(element == null){
       throw new NullPointerException("please enter a non-null element");}
+    if(size() == 0){
+      resize();
+      start = 0;
+      end = 1;
+      size = 1;
+      data[0] = element;
+      return;
+      }
     if(start == 0){
       resize();
       start = size();}
     start ++;
+    size ++;
     data[start] = element;}
 
   public void addLast(E element){
     if(element == null){
       throw new NullPointerException("please enter a non-null element");}
+    if(size() == 0){
+      resize();
+      start = 0;
+      end = 1;
+      data[1] = element;
+      size = 1;
+      return;}
+    if(end == data.length){
+      resize();}
     end = (end + 1) % (size());
+    size ++;
     data[end] = element;}
 
   public void resize(){
-    E[] temp = (E[])new Object[size() * 2];
+    E[] temp = (E[])new Object[(size() + 1) * 2];
     for(int i = 0; i < data.length; i++){
-      temp[i] = data[i];}}
+      temp[i] = data[i];}
+    data = temp;
+    }
 }
